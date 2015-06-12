041689fb081affa2cad4f6ebbbc6acd78ca6a0a1
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-06-12 17:50:35 +0200

    add press link snippet

--- core/models.py
+++ core/models.py
@@ -1,5 +1,27 @@
 from wagtail.wagtailcore.models import Page
+from django.db import models
+
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailsnippets.models import register_snippet
 
 
 class HomePage(Page):
     pass
+
+
+@register_snippet
+class PressLink(models.Model):
+    url = models.URLField(null=True)
+    source = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    date = models.DateField("Post date")
+
+    panels = [
+        FieldPanel('url'),
+        FieldPanel('source'),
+        FieldPanel('title'),
+        FieldPanel('date')
+    ]
+
+    def __unicode__(self):
+        return self.source + ', ' + self.title
