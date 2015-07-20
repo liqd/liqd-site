ebc43e2198f39915a56331d6a418536a0d6915fb
Caroline Clifford <me@carolineclifford.com>
2015-07-20 17:00:49 +0200

    Changed admin language to en, sorry!

--- liquidsite/settings/base.py
+++ liquidsite/settings/base.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'liquidsite.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'de-de'
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Europe/Berlin'
 USE_I18N = True
 USE_L10N = True
