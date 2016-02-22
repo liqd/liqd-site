2969f7545ae744093fbc200184197856ec4ea705
meisterkaiser <tom@meisterkaiser.de>
2016-02-22 23:32:14 +0100

    - new page type TextPage - translation added for HomePage and TextPage

--- core/models.py
+++ core/models.py
@@ -10,23 +10,106 @@ from wagtail.wagtailcore import blocks
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 
+from wagtail_modeltranslation.models import TranslationMixin
+from wagtail.wagtailadmin.edit_handlers import TabbedInterface, ObjectList, MultiFieldPanel
 
-class HomePage(Page):
+from wagtail.contrib.settings.models import BaseSetting, register_setting
+
+
+
+class HomePage(TranslationMixin, Page):
+    heading = models.CharField(max_length=255, default="")
+    intro = RichTextField(blank=True)
     body = StreamField([
         ('heading', blocks.CharBlock(classname="full title", icon="title")),
         ('paragraph', blocks.RichTextBlock(icon="pilcrow")),
         ('image', ImageChooserBlock(icon="image")),
     ], null=True)
+    content_panels = [
+    ]
+    de_content_panels = [
+        FieldPanel('heading_de'),
+        FieldPanel('intro_de'),
+        StreamFieldPanel('body_de'),
+    ]
 
-HomePage.content_panels = [
-    StreamFieldPanel('body')
-]
+    en_content_panels = [
+        FieldPanel('heading_en'),
+        FieldPanel('intro_en'),
+        StreamFieldPanel('body_en'),
+    ]
+
+    promote_panels = [
+        FieldPanel('slug'),
+        MultiFieldPanel([
+            FieldPanel('seo_title_de'),
+            FieldPanel('search_description_de'),
+        ],
+        heading = "SEO settings de",
+        classname="collapsible"),
+        MultiFieldPanel([
+            FieldPanel('seo_title_en'),
+            FieldPanel('search_description_en'),
+        ],
+        heading = "SEO settings en",
+        classname="collapsible")
+    ]
+
+    edit_handler = TabbedInterface([
+        # ObjectList(content_panels, heading='Content'),
+        ObjectList(de_content_panels, heading='Content de'),
+        ObjectList(en_content_panels, heading='Content en'),
+        ObjectList(promote_panels, heading='Promote'),
+        ObjectList(Page.settings_panels, heading='Settings', classname="settings"),
+    ])
+
+
+class TextPage(TranslationMixin, Page):
+    
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+    ]
+
+    de_content_panels = [
+        FieldPanel('title_de'),
+        FieldPanel('body_de'),
+    ]
+
+    en_content_panels = [
+        FieldPanel('title_en'),
+        FieldPanel('body_en'),
+    ]
+
+    promote_panels = [
+        FieldPanel('slug'),
+        MultiFieldPanel([
+            FieldPanel('seo_title_de'),
+            FieldPanel('search_description_de'),
+        ],
+        heading = "SEO settings de",
+        classname="collapsible"),
+        MultiFieldPanel([
+            FieldPanel('seo_title_en'),
+            FieldPanel('search_description_en'),
+        ],
+        heading = "SEO settings en",
+        classname="collapsible")
+    ]
+
+    edit_handler = TabbedInterface([
+        # ObjectList(content_panels, heading='Content'),
+        ObjectList(de_content_panels, heading='Content de'),
+        ObjectList(en_content_panels, heading='Content en'),
+        ObjectList(promote_panels, heading='Promote'),
+        ObjectList(Page.settings_panels, heading='Settings', classname="settings"),
+    ])
 
 
 class PressPage(Page):
     intro = RichTextField(blank=True)
 
-    subpage_types = []
+    #subpage_types = []
 
 PressPage.content_panels = [
     FieldPanel('title', classname="full title"),
@@ -50,3 +133,11 @@ class PressLink(models.Model):
 
     def __unicode__(self):
         return self.source + ', ' + self.title
+
+
+@register_setting
+class LiqdSettings(BaseSetting):
+    liqd_preliminary_site = models.BooleanField(
+        default=False,
+        help_text="Phase One of the new site, this setting can be deleted when the full site is ready to go live",
+    )
