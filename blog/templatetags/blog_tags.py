71336beafef7ad7b44c6eed2dd682c375471dc1a
Magdalena Noffke <magdalenanoffke@MagMac.fritz.box>
2016-03-29 20:51:20 +0200

    add template tag

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from blog.models import BlogPage
+
+register = template.Library()
+
+@register.inclusion_tag('blog/tags/blogentries.html', takes_context=True)
+def blog_entries(context):
+    return {
+        'blog_entries': BlogPage.objects.all().live().order_by('-date')[:2],
+        'request': context['request']
+    }
