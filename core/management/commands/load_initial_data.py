2992971db53c59fb79a672904b7e5911945ee244
Magdalena Noffke <magdalenanoffke@magmac.speedport_w_724v_typ_a_05011603_00_009>
2016-04-07 12:41:15 +0200

    dont create user

--- core/management/commands/load_initial_data.py
+++ core/management/commands/load_initial_data.py
@@ -8,5 +8,4 @@ class Command(BaseCommand):
     help = u'Load initial Data to Database'
 
     def handle(self, *args, **options):
-        # User.objects.create_superuser(username='admin', password='admin', email='admin@test.de')
         call_command('loaddata', 'initial_data.json', verbosity=0)
