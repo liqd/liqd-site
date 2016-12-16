ab1036ee0d3b519bec1ecb506e4a34d43eeb84dc
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-12-16 15:54:30 +0100

    add person snippets

---
+++ persons/migrations/0004_personsnippet.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import wagtail.wagtailcore.fields
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailimages', '0013_make_rendition_upload_callable'),
+        ('persons', '0003_auto_20160407_1910'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='PersonSnippet',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('first_name', models.CharField(max_length=255)),
+                ('last_name', models.CharField(max_length=255)),
+                ('email', models.EmailField(max_length=254, blank=True)),
+                ('motto_de', wagtail.wagtailcore.fields.RichTextField(blank=True)),
+                ('motto_en', wagtail.wagtailcore.fields.RichTextField(blank=True)),
+                ('area_de', models.CharField(max_length=256)),
+                ('area_en', models.CharField(max_length=256)),
+                ('image', models.ForeignKey(related_name='+', to='wagtailimages.Image', null=True, blank=True, on_delete=django.db.models.deletion.SET_NULL)),
+            ],
+        ),
+    ]
--- persons/models.py
+++ persons/models.py
@@ -2,13 +2,19 @@ from django.db import models
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailcore import blocks as core_blocks
+from wagtail.wagtailsnippets import blocks as snippet_blocks
 from wagtail.wagtailsearch import index
+from wagtail.wagtailsnippets.models import register_snippet
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailadmin.edit_handlers import MultiFieldPanel
 from wagtail.wagtailadmin.edit_handlers import InlinePanel
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.http import HttpResponse
+from wagtail.wagtailadmin.edit_handlers import TabbedInterface, ObjectList
+
+from contrib.translations.translations import TranslatedField
 
 
 VORSTAND = 'Vorstand'
@@ -42,10 +48,19 @@ class ContactFields(models.Model):
 class PersonPage(Page, ContactFields):
     parent_page_types = ['persons.PersonIndexPage']
     subpage_types = []
+@register_snippet
+class PersonSnippet(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     biography = RichTextField(blank=True)
     area = models.CharField(max_length=256, choices=AREAS_CHOICES)
+    email = models.EmailField(blank=True)
+
+    motto_de = RichTextField(blank=True)
+    motto_en = RichTextField(blank=True)
+
+    area_de = models.CharField(max_length=256)
+    area_en = models.CharField(max_length=256)
     image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -93,6 +108,9 @@ class PersonPage(Page, ContactFields):
         index.SearchField('last_name'),
         index.SearchField('intro'),
         index.SearchField('biography'),
+    area = TranslatedField(
+        'area_de',
+        'area_en'
     )
 
 PersonPage.content_panels = [
@@ -116,7 +134,13 @@ class PersonIndexPage(Page):
 
     search_fields = Page.search_fields + (
         index.SearchField('intro'),
+    motto = TranslatedField(
+        'motto_de',
+        'motto_en'
     )
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
 
     @property
     def persons(self):
