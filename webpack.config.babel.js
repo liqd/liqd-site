e8daae98fa4526235f268be8bf8768d59366841f
vellip <vellerphilipp@googlemail.com>
2017-06-07 17:05:56 +0200

    add fonts, remove glyphicons

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -44,7 +44,7 @@ module.exports = {
         loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }),
       },
       {
-        test: /\.(woff2?|ttf|eot|svg|jpg|png|gif|swf)(\?.*)?$/,
+        test: /\.(woff2?|ttf|eot|svg|jpg|png|gif|swf|otf)(\?.*)?$/,
         loader: 'file-loader',
       },
       {
