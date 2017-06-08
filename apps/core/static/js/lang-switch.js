2a31e82ed014ed2548c4c3a37a60fc0d3cb6594e
vellip <vellerphilipp@googlemail.com>
2017-06-08 16:23:54 +0200

    add animation handling for lang-switch

--- apps/core/static/js/lang-switch.js
+++ apps/core/static/js/lang-switch.js
@@ -2,8 +2,19 @@ $(function () {
   const $langSwitch = $('#lang-switch')
   const $langActive = $langSwitch.find('.lang-switch__active')
   const $langList = $langSwitch.find('.lang-switch__list')
+  let isOpen = false
 
   $langActive.on('click', (e) => {
-    $langList.toggleClass('lang-switch__list--active')
+    if (!isOpen) {
+      $langList.css({'display': 'block'})
+      // trigger layout so transition works initially
+      $langList.css('opacity')
+      $langList.addClass('lang-switch__list--active')
+    } else {
+      $langList.removeClass('lang-switch__list--active')
+      $langList.one('transitionEnd', () => $langList.removeAttr('style'))
+    }
+    // toggle variable
+    isOpen = !isOpen
   })
 })
