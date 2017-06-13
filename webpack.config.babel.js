22b3a510d84772add6ffded2950864ef8580da75
vellip <vellerphilipp@googlemail.com>
2017-06-13 11:08:52 +0200

    fix es6 error with bootstrap

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -50,7 +50,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!bootstrap)/,
         loader: 'babel-loader',
         query: {
           cacheDirectory: true,
