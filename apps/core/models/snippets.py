7e3db394261244059c8709035ead1a761764c2ac
vellip <vellerphilipp@googlemail.com>
2017-06-13 14:09:13 +0200

    remove icon from blog categoryy

--- apps/core/models/snippets.py
+++ apps/core/models/snippets.py
@@ -74,16 +74,11 @@ class NavigationMenuItem(Orderable, MenuItem):
 class BlogCategory(models.Model):
     name_en = models.CharField(max_length=255)
     name_de = models.CharField(max_length=255, blank=True, null=True)
-    icon = models.ForeignKey(
-        'wagtailimages.Image', null=True, blank=True,
-        on_delete=models.SET_NULL, related_name='+'
-    )
     translated_name = TranslatedField('name_en', 'name_de')
 
     panels = [
         FieldPanel('name_en'),
         FieldPanel('name_de'),
-        ImageChooserPanel('icon'),
     ]
 
     def __str__(self):
