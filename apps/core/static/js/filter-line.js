da7399abc4d555ed0bbae1869edb421bc88bfcdb
vellip <vellerphilipp@googlemail.com>
2017-06-15 11:00:51 +0200

    allow to disable filter line

--- apps/core/static/js/filter-line.js
+++ apps/core/static/js/filter-line.js
@@ -2,12 +2,25 @@
 
 $(function () {
   let $triggers = $('.filter-line__current')
+  let $window = $(window)
 
   $triggers.on('click', function () {
     let $this = $(this)
     let $list = $this.next()
 
-    $list.toggleClass('filter-line__list--active')
-    $this.toggleClass('filter-line__current--active')
+    $list.addClass('filter-line__list--active')
+    $this.addClass('filter-line__current--active')
+
+    // make sure only one event is set
+    $window.off('click.filter-hide')
+    $window.on('click.filter-hide', function (e) {
+      let $target = $(e.target)
+      if (!$target.parents('.filter-line').length) {
+        $list.removeClass('filter-line__list--active')
+        $this.removeClass('filter-line__current--active')
+        // remove event when not needed anymore
+        $window.off('click.filter-hide')
+      }
+    })
   })
 })
