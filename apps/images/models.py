d90247ce1f04c59192454d3892e303b0cbbf82af
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-06-19 15:28:40 +0200

    fix linting errors

--- apps/images/models.py
+++ apps/images/models.py
@@ -1,8 +1,7 @@
 from django.db import models
+from wagtail.wagtailimages.models import (AbstractImage, AbstractRendition,
+                                          Image)
 
-from wagtail.wagtailimages.models import AbstractImage
-from wagtail.wagtailimages.models import AbstractRendition
-from wagtail.wagtailimages.models import Image
 
 class CustomImage(AbstractImage):
 
