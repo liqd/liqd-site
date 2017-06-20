1d0105ab340b9096c03d3c142b10427468a75076
Magdalena Noffke <magdalena.noffke@liqd.de>
2017-06-20 14:09:09 +0200

    add styling and js for add more button

---
+++ apps/core/static/js/load-more.js
@@ -0,0 +1,34 @@
+/* global $ */
+
+$(function () {
+
+    var count = 2
+
+    $('.load-more').on("click", function (event) {
+        event.preventDefault()
+        loadProjects(count)
+        count ++
+
+    })
+
+    function loadProjects(pageNumber){
+        $.ajax({
+            url: "?page=" + count,
+            type:'GET',
+            success: function(html){
+                $('.project-list').append(html)
+                $.ajax({
+                    url: "?page=" + (count+1),
+                    type:'GET',
+                    error: function(xhr, status, errorThrown){
+                        if(xhr.status == "404") {
+                            $('.load-more').hide()
+                        }
+                    }
+                })
+            }
+        })
+        return false
+    }
+
+});
