74ff669c13c941cf659ed541ac54620f334ff245
vellip <vellerphilipp@googlemail.com>
2017-07-05 12:18:22 +0200

    remove unwanted hallo buttons

---
+++ apps/core/wagtail_hooks.py
@@ -0,0 +1,15 @@
+from django.utils.html import format_html
+
+from wagtail.wagtailcore import hooks
+
+
+@hooks.register('insert_editor_js')
+def editor_js():
+    return format_html(
+        """
+        <script>
+            delete halloPlugins.halloheadings;
+            delete halloPlugins.halloformat;
+        </script>
+        """
+    )
