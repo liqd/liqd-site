a432076501b96ffa34650a1ae1d053ba00c4ad50
Phillippa <p.morland@liqd.net>
2020-06-22 16:40:23 +0200

    academy_index: adding pagination - add pagination from a+ and add template tag

--- apps/core/templatetags/core_tags.py
+++ apps/core/templatetags/core_tags.py
@@ -31,3 +31,12 @@ def do_translate_url(context, language):
     except Http404:
         url = '/' + language + '/'
     return url
+
+
+@register.simple_tag
+def combined_url_parameter(request_query_dict, **kwargs):
+    combined_query_dict = request_query_dict.copy()
+    for key in kwargs:
+        combined_query_dict.setlist(key, [kwargs[key]])
+    encoded_parameter = '?' + combined_query_dict.urlencode()
+    return encoded_parameter
