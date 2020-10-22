168ab3f795f0a7cf38c7eceb9323ffb1da3ef38e
Phillippa <p.morland@liqd.net>
2020-10-22 14:52:43 +0200

    package.json//webpack.common: update packages and configs

--- webpack.common.js
+++ webpack.common.js
@@ -47,10 +47,11 @@ module.exports = {
           {
             loader: 'postcss-loader',
             options: {
-              ident: 'postcss',
-              plugins: [
-                require('autoprefixer')
-              ]
+              postcssOptions: {
+                plugins: [
+                  require('autoprefixer')
+                ]
+              }
             }
           },
           {
