7ccc1fe6558a46772e4a837848a7e40fa32be432
phillimorland <phillippamorland@gmail.com>
2021-02-18 18:35:25 +0100

    package.json: update required packages

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,14 +3,13 @@ const common = require('./webpack.common.js')
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
+  devtool: 'source-map',
   optimization: {
     minimizer: [
       new TerserPlugin({
-        sourceMap: true,
         parallel: true,
         terserOptions: {
           ecma: 5,
-          arrows: false
         }
       })
     ]
