f494301c6f3a7be0fde3e324e8173d65d6681a26
Julian Dehm <j.dehm@liqd.net>
2022-08-18 10:17:36 +0200

    add github actions and issue templates

---
+++ website_wagtail/settings/ci.py
@@ -0,0 +1,12 @@
+from .dev import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'NAME': 'django',
+        'TEST': {
+            'NAME': 'django_test'
+        },
+    }
+}
