b473713cf3f5470c417ebfc1d8075ce86e4493c7
Kha <k.tran@liqd.net>
2022-08-23 15:21:18 +0200

    academy/models.py & /blocks.py & block_topic_teaser.html & _blocks.scss

--- apps/academy/blocks.py
+++ apps/academy/blocks.py
@@ -1,6 +1,10 @@
-from wagtail.blocks import (CharBlock, ListBlock, PageChooserBlock,
-                            RichTextBlock, StructBlock, TextBlock, URLBlock)
+from django.template.defaulttags import register
+from wagtail.blocks import (CharBlock, ChoiceBlock, ListBlock,
+                            PageChooserBlock, RichTextBlock, StructBlock,
+                            TextBlock, URLBlock)
 from wagtail.images.blocks import ImageChooserBlock
+
+from apps.academy.choices import TOPIC_CHOICES
 
 
 class ChallengeStepBlock(StructBlock):
@@ -82,3 +86,37 @@ class AcademyCallToActionBlock(StructBlock):
         icon = 'plus-inverse'
         help_text = 'Add 1 or 2 column teaser with black '
         'background and white writing'
+
+
+class TopicBlock(StructBlock):
+    topic_category = ChoiceBlock(
+        choices=TOPIC_CHOICES,
+        required=True,
+        help_text='Select a topic'
+    )
+    topic_text = CharBlock(required=True, max_length=110)
+    topic_link_text = CharBlock(required=True, max_length=40)
+
+
+class TopicBlockList(StructBlock):
+    title = CharBlock(required=True, max_length=74)
+    topics = ListBlock(TopicBlock(), min_num=3, max_num=3)
+    topic_url = PageChooserBlock(
+        required=True,
+        page_type='academy.AcademyIndexPage'
+    )
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        context['topics_dict'] = dict(TOPIC_CHOICES)
+        return context
+
+    class Meta:
+        template = 'academy/blocks/block_topic_teaser.html'
+        icon = 'grip'
+        label = '3 topic teasers'
+
+
+@register.filter
+def get_category_name(dict, key):
+    return dict.get(key)
